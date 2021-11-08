81ffeffb3dd248e524cfe3a56bc9a59a07439d31
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-11-08 22:02:20 +0200

    init

--- SwiftUIPullToRefresh/ContentView.swift
+++ SwiftUIPullToRefresh/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var posts = [
+        NewsItem(id: 0, title: "Want the latest posts?", body: "Pull to refresh!")
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(posts) { item in
+                VStack(alignment: .leading) {
+                    Text(item.title)
+                        .font(.headline)
+                    Text(item.body)
+                        .foregroundColor(.secondary)
+                }
+            }
+            .refreshable {
+                do {
+                    let url = URL(string: "https://jsonplaceholder.typicode.com/posts")!
+                    let (data, _) = try await URLSession.shared.data(from: url)
+                    posts = try JSONDecoder().decode([NewsItem].self, from: data)
+                } catch {
+                    posts = []
+                }
+            }
+        }
     }
 }
 
@@ -18,4 +40,10 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct NewsItem: Decodable, Identifiable {
+    let id: Int
+    let title: String
+    let body: String
 }
